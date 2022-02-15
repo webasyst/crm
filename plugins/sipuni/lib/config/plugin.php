8e4cd991d8d1bc53e4623ef332c908cd1601a322
Leonix <leonix@motd.ru>
2022-02-15 14:55:44 +0300

    crm/plugins/sipuni v.1.0.2   * Minor localization improvements.

--- plugins/sipuni/lib/config/plugin.php
+++ plugins/sipuni/lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array(
     'name'                => 'Sipuni',
     'img'                 => 'img/sipuni.png',
-    'version'             => '1.0.1',
+    'version'             => '1.0.2',
     'vendor'              => 'webasyst',
     'frontend'            => true,
     'custom_settings_url' => '?plugin=sipuni&module=settings',
